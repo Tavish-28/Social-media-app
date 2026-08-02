601cf70d4f27b23c3ec7245af39f086b1078b57d
Tavish <golchatavish@gmail.com>
2026-08-02 10:24:01 +0530

    fixed the api issue of mongodb

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,11 +1,14 @@
 import { Inngest } from "inngest";
-import { serve } from "inngest/express";
+// import { serve } from "inngest/express";
+import User from "../models/User.js";
+import dotenv from "dotenv";
+
+// dotenv.config();
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "my-app" });
 const syncUserCreation = inngest.createFunction(
-  { id: "sync-user-from-clerk" },
-  { event: "clerk/user.created" },
+  { id: "sync-user-from-clerk", triggers: [{ event: "clerk/user.created" }] },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
@@ -30,12 +33,14 @@ const syncUserCreation = inngest.createFunction(
   },
 );
 const syncUserUpdate = inngest.createFunction(
-  { id: "sync-user-from-clerk" },
-  { event: "clerk/user.updated" },
+  {
+    id: "sync-user-update-from-clerk",
+    triggers: [{ event: "clerk/user.updated" }],
+  },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
-    
+
     const updatedUserData = {
       // _id: id,
       email: email_addresses[0].email_address,
@@ -47,8 +52,10 @@ const syncUserUpdate = inngest.createFunction(
   },
 );
 const syncUserDeletion = inngest.createFunction(
-  { id: "delete-user-from-clerk" },
-  { event: "clerk/user.deleted" },
+  {
+    id: "delete-user-from-clerk",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
   async ({ event }) => {
     const { id } = event.data;
     await User.findByIdAndDelete({ _id: id });
--- server/server.js
+++ server/server.js
@@ -1,19 +1,34 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import { config } from "dotenv";
 import connectDB from "./configs/db.js";
-import { inngest, functions } from "./routes/ingest.js";
-import serve from "inngest/express";
+
+import { inngest, functions } from "./inngest/index.js";
+import { serve } from "inngest/express";
+
+dotenv.config();
 
 const app = express();
+
 await connectDB();
 
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => res.send("Server is running"));
-app.use("/api/inngest", serve({ client: inngest, fuctions }));
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions,
+  }),
+);
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
